31fd52935d355fb240962041213ff09fb93d94f5
wumeng.wm <wumeng.wm@alibaba-inc.com>
2016-01-17 18:57:16 +0800

    Add the basic static route

--- demo.js
+++ demo.js
@@ -1,10 +1,6 @@
 let Airdog = require('./lib/Application')
 let app = new Airdog({
-  static: 'user'
-})
-
-app.get('/user2',async function(){
-  this.body = await this.read('demo2.js')
+  static: '/'
 })
 
 app.listen(8080)
