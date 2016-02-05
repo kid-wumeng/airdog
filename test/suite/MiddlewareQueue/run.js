955d33b02ac603be856ee1e7c285b6f7d85e88f8
wumeng.wm <wumeng.wm@alibaba-inc.com>
2016-02-05 16:43:38 +0800

    Change the MiddleQueue.add

--- test/suite/MiddlewareQueue/run.js
+++ test/suite/MiddlewareQueue/run.js
@@ -1,95 +1,122 @@
+let Middleware      = Airdog.import('Middleware')
 let MiddlewareQueue = Airdog.import('MiddlewareQueue')
 
 
 
-test('Run All Normal-Functions', function(){
+test('Run All Normal-Functions', function()
+{
   let flag = new Flag(1)
+
+  function fn1(){ flag.mark(1) }
+  function fn2(){ flag.mark() }
+  function fn3(){ flag.mark() }
+  
   let midQue = new MiddlewareQueue
-  midQue.add(function(){
-    flag.mark(1)
-  })
-  midQue.add(function(){
-    flag.mark()
-  })
-  midQue.add(function(){
-    flag.mark()
-  })
+  midQue.add(new Middleware(fn1))
+  midQue.add(new Middleware(fn2))
+  midQue.add(new Middleware(fn3))
   midQue.run()
   flag.assert()
 })
 
 
 
-test('Run All Normal-Functions & Async-Functions', async function(done){
+test('Run All Normal-Functions & Async-Functions', async function(done)
+{
   let flag = new Flag(3)
-  let midQue = new MiddlewareQueue
-  midQue.add(async function(){
+  
+  async function fn1(){
     flag.mark(1)
     await this.next
     flag.mark(3)
     flag.assert()
     done()
-  })
-  midQue.add(function(){
+  }
+  
+  function fn2(){
     flag.mark(2)
-  })
-  midQue.add(async function(){
+  }
+  
+  async function fn3(){
     flag.mark()
     await this.next
     flag.mark()
-  })
+  }
+  
+  let midQue = new MiddlewareQueue
+  midQue.add(new Middleware(fn1))
+  midQue.add(new Middleware(fn2))
+  midQue.add(new Middleware(fn3))
   await midQue.run()
 })
 
 
 
-test('Run All Async-Functions', async function(done){
+test('Run All Async-Functions', async function(done)
+{
   let flag = new Flag(6)
-  let midQue = new MiddlewareQueue
-  midQue.add(async function(){
+  
+  async function fn1(){
     flag.mark(1)
     await this.next
     flag.mark(6)
     flag.assert()
     done()
-  })
-  midQue.add(async function(){
+  }
+  
+  async function fn2(){
     flag.mark(2)
     await this.next
     flag.mark(5)
-  })
-  midQue.add(async function(){
+  }
+  
+  async function fn3(){
     flag.mark(3)
     await this.next
     flag.mark(4)
-  })
+  }
+  
+  let midQue = new MiddlewareQueue
+  midQue.add(new Middleware(fn1))
+  midQue.add(new Middleware(fn2))
+  midQue.add(new Middleware(fn3))
   await midQue.run()
 })
 
 
 
-test('Run 3/4 Async-Functions', async function(done){
+test('Run 3/4 Async-Functions', async function(done)
+{
   let flag = new Flag(5)
-  let midQue = new MiddlewareQueue
-  midQue.add(async function(){
+  
+  async function fn1(){
     flag.mark(1)
     await this.next
     flag.mark(5)
     flag.assert()
     done()
-  })
-  midQue.add(async function(){
+  }
+  
+  async function fn2(){
     flag.mark(2)
     await this.next
     flag.mark(4)
-  })
-  midQue.add(async function(){
+  }
+  
+  async function fn3(){
     flag.mark(3)
-  })
-  midQue.add(async function(){
+  }
+  
+  async function fn4(){
     flag.mark()
     await this.next
     flag.mark()
-  })
+  }
+  
+  let midQue = new MiddlewareQueue
+  midQue.add(new Middleware(fn1))
+  midQue.add(new Middleware(fn2))
+  midQue.add(new Middleware(fn3))
+  midQue.add(new Middleware(fn4))
   await midQue.run()
 })
