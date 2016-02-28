482baa08d31ee1c2c8aeb1517fe8ce0e50a2d4f5
wumeng.wm <wumeng.wm@alibaba-inc.com>
2016-02-29 01:39:48 +0800

    Add Airdog.Static test-cases

--- test/suite/_middleware/Static/index.js
+++ test/suite/_middleware/Static/index.js
@@ -1,9 +1,55 @@
+let fs = require('fs')
 let request = require('request')
+
+
 
 test('Get HTML', function(done){
   request.get('http://127.0.0.1:8080/_test.html', function(err, res, body){
-    console.log(res.headers['Content-Length']);
+    res.headers['content-type'].should.equal('text/html')
+    res.headers.should.not.include.keys('content-encoding')
     body.should.equal('<!DOCTYPE html>')
+    done()
+  })
+})
+
+
+
+test('Get HTML ( gzip )', function(done){
+  let headers = {'Accept-Encoding': 'gzip, deflate, lzma, sdch'}
+  let options = { headers: headers }
+  request.get('http://127.0.0.1:8080/_test.html', options, function(err, res, body){
+    res.headers['content-type'].should.equal('text/html')
+    res.headers['content-encoding'].should.equal('gzip')
+    // @TODO unEncoding
+    // body.should.equal('<!DOCTYPE html>')
+    done()
+  })
+})
+
+
+
+test('Get HTML ( deflate )', function(done){
+  let headers = {'Accept-Encoding': 'deflate, lzma, sdch'}
+  let options = { headers: headers }
+  request.get('http://127.0.0.1:8080/_test.html', options, function(err, res, body){
+    res.headers['content-type'].should.equal('text/html')
+    res.headers['content-encoding'].should.equal('deflate')
+    // @TODO unEncoding
+    // body.should.equal('<!DOCTYPE html>')
+    done()
+  })
+})
+
+
+
+test('Get PNG ( unvalid-gzip )', function(done){
+  let headers = {'Accept-Encoding': 'gzip, deflate, lzma, sdch'}
+  let options = { headers: headers }
+  request.get('http://127.0.0.1:8080/_test.png', options, function(err, res, body){
+    res.headers['content-type'].should.equal('image/png')
+    res.headers.should.not.include.keys('content-encoding')
+    let image = fs.readFileSync(`${__dirname}/_test.png`, 'utf-8')
+    body.should.equal(image)
     done()
   })
 })
