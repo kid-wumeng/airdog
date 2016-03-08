83543a10937f53bf0510b521bc507417648bfc7c
wumeng.wm <wumeng.wm@alibaba-inc.com>
2016-03-08 20:16:52 +0800

    Handle the session timeout

--- de/de.js
+++ de/de.js
@@ -1,28 +1,29 @@
 "use strict"
 
-let s = new Airdog({})
-
-// s.get('/', function(){
-//   let username = this.session.username
-//   if(!username){
-//     this.session.username = 'kiddik'
-//     this.session.age = 18
-//   }
-//   this.removeSession('username')
-//   this.body = this.session
-// })
-
+let s = new Airdog({
+  'session': {
+    'max-age': 10000,
+    'inspect-time': 10000
+  }
+})
 
 s.get('/', function(){
-  this.body = 'kidd'
+  let username = this.session.username
+  if(!username){
+    this.session.username = 'kiddik'
+    this.session.age = 18
+  }
+  this.removeSession()
+  this.body = this.session
 })
+
 
 s.listen(8080)
 
 request.get({
   url: 'http://127.0.0.1:8080/'
 }, function(err, res, body){
-  console.log(body);
+  // console.log(body);
   s.close()
 })
 
--- test/suite/_middleware/Session/index.js
+++ test/suite/_middleware/Session/index.js
@@ -1,18 +1,16 @@
 "use strict"
 
 
-
 const request = require('request')
-
 
 
 test('Get and Set Session', function(done){
   client.get('/get-set', function(res, body){
     body.should.equal('')
 
-    let co1 = request.cookie(res.headers['set-cookie'][0])
+    let co  = request.cookie(res.headers['set-cookie'][0])
     let jar = request.jar()
-    jar.setCookie(co1, 'http://127.0.0.1:8080')
+    jar.setCookie(co, 'http://127.0.0.1:8080')
     let options = { jar: jar }
 
     client.get('/get-set', options, function(res, body){
@@ -22,6 +20,43 @@ test('Get and Set Session', function(done){
       }))
       done()
     })
+  })
+})
+
+
+test('Get and Set Session ( Timeout )', function(done){
+  let app = new Airdog({
+    'session': {
+      'max-age': 10,
+      'inspect-time': 10
+    }
+  })
+  app.get('/', function(){
+    let name = this.session.name
+    if(!name)
+      this.session.name = 'kid'
+    this.body = name
+  })
+  app.listen(8081)
+  
+  request.get('http://127.0.0.1:8081', function(err, res, body)
+  {
+    body.should.equal('')
+    
+    let co  = request.cookie(res.headers['set-cookie'][0])
+    let jar = request.jar()
+    jar.setCookie(co, 'http://127.0.0.1:8080')
+    let options = { jar: jar }
+    
+    setTimeout(function(){
+      request.get('http://127.0.0.1:8081', options, function(err, res, body)
+      {
+        body.should.equal('')
+        app.close()
+        done()
+      })
+    }, 20)
+    
   })
 })
 
