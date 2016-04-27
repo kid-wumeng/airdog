19dd887ab8874f0f771f9cc8f6a34d97828e216a
KID <kid.wumeng@gmail.com>
2016-04-28 04:33:38 +0800

    Finished the Kit.formatPath

--- test/suite/Kit/index.js
+++ test/suite/Kit/index.js
@@ -7,10 +7,10 @@ test('Merge Normal-Object', function(){
   let src = { name: 'kid', age: 18 }
   let dest = {}
   let res = Kit.merge(dest, src)
-  
+
   dest.name.should.equal('kid')
   dest.age.should.equal(18)
-  
+
   res.name.should.equal('kid')
   res.age.should.equal(18)
 })
@@ -21,12 +21,12 @@ test('Merge Class\'s this', function(){
   function Class(src){
     Kit.merge(this, src)
   }
-  
+
   let obj = new Class({
     name: 'kid',
     age: 18
   })
-  
+
   obj.name.should.equal('kid')
   obj.age.should.equal(18)
 })
@@ -47,4 +47,10 @@ test('Is Empty-Object', function(){
   let o2 = { name: 'kid' }
   Kit.isEmptyObject(o1).should.true
   Kit.isEmptyObject(o2).should.false
+})
+
+
+
+test('Format Path', function(){
+  Kit.formatPath('//api/user///login//').should.equal('/api/user/login/')
 })
