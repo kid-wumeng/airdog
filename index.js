1414d3a900ebe9452afe8ec8ef19946ea240b531
KID <kid.wumeng@gmail.com>
2016-06-20 18:59:12 +0800

    0.3

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('hello world')
+
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+//
+//
+// app.use(require('express').static('public'))
+// app.get('/', function(req, res){
+//   res.sendfile('index.html');
+// });
+//
+//
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     socket.broadcast.emit('chat message', msg)
+//   });
+// });
+//
+//
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
---
+++ util/string.js
@@ -0,0 +1,3 @@
+exports.red = function(string){
+  return `\x1b[31m${string}\x1b[0m`
+}
